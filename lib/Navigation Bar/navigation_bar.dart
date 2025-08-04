6cb4873af3e3d70ddd81eb23936f6b51bd06dc1c
Tushar Saharan <82592709+tusharsharn@users.noreply.github.com>
2025-08-04 14:57:06 +0530

    navigation_bar.dart

--- lib/Navigation Bar/navigation_bar.dart
+++ lib/Navigation Bar/navigation_bar.dart
@@ -3,10 +3,8 @@ import 'package:flutter_basics/presentation/Fav%20Page/Fav%20Page.dart';
 import 'package:flutter_basics/presentation/Profile%20Page/profile_page.dart';
 import 'package:flutter_basics/presentation/choose%20mode/pages/home_page.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 import '../core/configs/assets/app_vectors.dart';
 import '../core/configs/themes/app_colors.dart';
-// import your AppVectors.dart here
 
 class NavigationBarPage extends StatefulWidget {
   const NavigationBarPage({Key? key}) : super(key: key);
